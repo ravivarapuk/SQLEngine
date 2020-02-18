5d838401dfe50399ff616cf13ce30c5bd5f1da1e
Karthik Ravivarapu <22068548+ravivarapuk@users.noreply.github.com>
2020-02-18 10:37:03 +0530

    fixing module errors

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import sys
-from src.deps.sql_parser import _init_metadata, _parse_query, _get_op_tbl, _print_table
+from src.deps import sql_parser as sp
 
 # directories set-up
 DB_DIR = "../req_files/"
@@ -7,17 +7,17 @@ META_FILE = "../req_files/metadata.txt"
 
 
 def main():
-    _init_metadata(META_FILE)
+    sp._init_metadata(META_FILE)
     if len(sys.argv) != 2:
         print("ERROR : invalid args")
         print("USAGE : python {} '<sql query>'".format(sys.argv[0]))
         exit(-1)
     q = sys.argv[1]
 
-    qdict = _parse_query(q)
-    out_header, out_table = _get_op_tbl(qdict)
+    qdict = sp._parse_query(q)
+    out_header, out_table = sp._get_op_tbl(qdict)
 
-    _print_table(out_header, out_table)
+    sp._print_table(out_header, out_table)
 
 
 if __name__ == "__main__":
